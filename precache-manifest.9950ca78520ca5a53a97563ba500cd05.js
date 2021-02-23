86994f65246e4ceb89d5d0940f188aedd6f96277
DarthAmun <amon.loen@gmail.com>
2021-02-23 10:59:11 +0100

    Updates

---
+++ precache-manifest.9950ca78520ca5a53a97563ba500cd05.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb02cb1a9cac5a68b905fb1a34b7d3d9",
+    "url": "/SkirmishTome-pwa/index.html"
+  },
+  {
+    "revision": "da08234ea509073ba6e3",
+    "url": "/SkirmishTome-pwa/static/css/main.a4aed14a.chunk.css"
+  },
+  {
+    "revision": "e8fcc7512617d0bdf4f1",
+    "url": "/SkirmishTome-pwa/static/js/0.d00e8a0a.chunk.js"
+  },
+  {
+    "revision": "709f26f7a60b6b020c81",
+    "url": "/SkirmishTome-pwa/static/js/10.50d57d11.chunk.js"
+  },
+  {
+    "revision": "4c7af31f7a1ffa699811",
+    "url": "/SkirmishTome-pwa/static/js/3.af5769bd.chunk.js"
+  },
+  {
+    "revision": "60f93ce92c073c15cea3beb40867e872",
+    "url": "/SkirmishTome-pwa/static/js/3.af5769bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ef93f980a1648332c6",
+    "url": "/SkirmishTome-pwa/static/js/4.2aa650be.chunk.js"
+  },
+  {
+    "revision": "4174aaad4e66d2f40b7d44de5e40b86b",
+    "url": "/SkirmishTome-pwa/static/js/4.2aa650be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf537eb0fb0a3252d0a",
+    "url": "/SkirmishTome-pwa/static/js/5.453a2612.chunk.js"
+  },
+  {
+    "revision": "9b256e277528a8c08b6a",
+    "url": "/SkirmishTome-pwa/static/js/6.52df3ee6.chunk.js"
+  },
+  {
+    "revision": "bed9abd1d542c7be3ee7",
+    "url": "/SkirmishTome-pwa/static/js/7.b2515f55.chunk.js"
+  },
+  {
+    "revision": "10bd1723483deb14e672",
+    "url": "/SkirmishTome-pwa/static/js/8.4630a872.chunk.js"
+  },
+  {
+    "revision": "d796212cc433b2aa21b1",
+    "url": "/SkirmishTome-pwa/static/js/9.4f1f199b.chunk.js"
+  },
+  {
+    "revision": "da08234ea509073ba6e3",
+    "url": "/SkirmishTome-pwa/static/js/main.e3020d46.chunk.js"
+  },
+  {
+    "revision": "6dd073374e807c7f0e8d",
+    "url": "/SkirmishTome-pwa/static/js/runtime-main.8a32665c.js"
+  },
+  {
+    "revision": "a0b12edf02c3f325bdf7975e77ac2194",
+    "url": "/SkirmishTome-pwa/static/media/logo192.a0b12edf.png"
+  }
+]);
